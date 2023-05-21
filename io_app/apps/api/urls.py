3cad5ba0bee0b26f4864b614d30a84f8255a7a36
zNitche <53448802+zNitche@users.noreply.github.com>
2023-05-21 13:58:22 +0200

    tweaked getting storage usage data

--- io_app/apps/api/urls.py
+++ io_app/apps/api/urls.py
@@ -7,4 +7,6 @@ app_name = "api"
 urlpatterns = [
     path("storage_usage/", views.storage_usage, name="storage_usage"),
     path("storage_usage_by_filetype/", views.storage_usage_by_filetype, name="storage_usage_by_filetype"),
+    path("processes/<str:user_id>/currently_running", views.running_processes_for_user,
+         name="running_processes_for_user"),
 ]
--- io_app/apps/api/views.py
+++ io_app/apps/api/views.py
@@ -1,16 +1,19 @@
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
-from io_app.utils import files_utils
+from io_app.utils import files_utils, tasks_utils
 
 
 @login_required
 @require_http_methods(["GET"])
 def storage_usage(request):
     used_space = files_utils.get_user_storage_size_in_mb(request.user.id)
+    free_space = round((request.user.private_storage_space - used_space), 2)
+
+    free_space = free_space if free_space >= 0 else 0
 
     return JsonResponse(data={
-        "free_space": round((request.user.private_storage_space - used_space), 2),
+        "free_space": free_space,
         "used_space": used_space
     })
 
@@ -24,3 +27,11 @@ def storage_usage_by_filetype(request):
     )
 
     return JsonResponse(data=storage_usage_percentage)
+
+
+@login_required
+@require_http_methods(["GET"])
+def running_processes_for_user(request, user_id):
+    processes_data = tasks_utils.get_tasks_data_for_user(user_id)
+
+    return JsonResponse(data={"processes": processes_data})
